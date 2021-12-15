10273afbdeba253f077da9dc717cf10ccf06c2bf
eyet010 <eyet010@clmns.co.kr>
2021-12-15 15:14:13 +0900

    todo

--- src/App.js
+++ src/App.js
@@ -1,55 +1,50 @@
 import { useState, useEffect } from "react";
 
 function App() {
-  /**
-   * state가 변화할때마다 모든 Component 재실행
-   * API 호출시에는 한번만 실행되어야 함
-   *
-   * useEffect(effect, deps)
-   * - 우리 코드가 딱 한번만 실행될 수 있도록 보호해줌 (State 변화해도)
-   * - effect(첫번째 인자): 딱 한번만 실행하고 싶은 코드
-   * - deps(두번째 인자) : [] 빈 배열
-   */
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((current) => current + 1);
-  const onChange = (event) => {
-    setKeyword(event.target.value);
-  };
-  console.log("I run all the time");
-  /**
-   * []
-   * : React 지켜볼게 없으니까 처음 한번만 실행됨
-   */
-  useEffect(() => {
-    console.log("I run only once");
-  }, []);
-  /**
-   * [keyword]
-   * : keyword가 변화할때 코드 한번 실행하게 함
-   * : keyword 변화할때 코드 실행하라고 알려줌
-   */
-  useEffect(() => {
-    if (keyword !== "" && keyword.length > 5) {
-      console.log("I run when 'keyword' changes");
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
     }
-  }, [keyword]);
-  useEffect(() => {
-    console.log("I run when 'counter' changes");
-  }, [counter]);
-  useEffect(() => {
-    console.log("I run when 'keyword' and 'counter' changes");
-  }, [keyword, counter]);
+    setToDo("");
+    /**
+     * toDos 배열에 추가하는 법
+     * - currentArray = toDos
+     */
+    setToDos((currentArray) => [toDo, ...currentArray]);
+  };
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search Here"
-      />
-      <h3>{counter}</h3>
-      <button onClick={onClick}>Click Me</button>
+      <h3>My ToDo {toDos.length}</h3>
+      <form onSubmit={onSubmit}>
+        <input
+          value={toDo}
+          onChange={onChange}
+          type="text"
+          placeholder="Write your To Do"
+        />
+        <button
+          style={{
+            color: "white",
+            fontWeight: "bold",
+            backgroundColor: "#f0a500",
+            border: "none",
+            borderRadius: "10px",
+            padding: "10px 10px",
+          }}
+        >
+          Add ToDo
+        </button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
